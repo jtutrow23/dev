22df9c25286c11dd0b0b358b96ea5ce044d5a52d
Justin Tutrow <jtutrow23@gmail.com>
2024-11-11 12:55:35 -0500

    Initial commit

---
+++ devTests/devTests.swift
@@ -0,0 +1,16 @@
+//
+//  devTests.swift
+//  devTests
+//
+//  Created by Justin Tutrow on 11/11/2024.
+//
+
+import Testing
+
+struct devTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
